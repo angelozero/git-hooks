c9b0a1a5103bb5d429745cf10e2bcdda0f434217
Angelo Ferreira <angelof@ciandt.com>
2019-07-15 15:49:40 -0300

    [ANGELO-123] tete

--- git-hooks/src/main/java/com/angelozero/App.java
+++ git-hooks/src/main/java/com/angelozero/App.java
@@ -6,6 +6,6 @@ package com.angelozero;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!!!!s!??");
+		System.out.println("Hello World!!!??");
 	}
 }
